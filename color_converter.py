4c9486cfcdd823bb8a0df07257dcbe95c31f9bb9
Oskar Gmerek <53402105+oskar-gmerek@users.noreply.github.com>
2023-08-29 21:49:18 +0100

    add rgb to cmyk converter

--- color_converter.py
+++ color_converter.py
@@ -1,3 +1,4 @@
+
 def toHex(number):
     if int(number) == 10:
         hex = 'A'
@@ -20,7 +21,7 @@ def rgbToHexHelper(number):
     temp_hex = int(hex * 16 )
     hex2 = int(number-temp_hex) 
     hex_digit1 = toHex(hex)
-    
+
     if hex2 > 9:
         hex_digit2 = toHex(hex2)
     else:
@@ -40,15 +41,63 @@ def rgbToHex(r, g, b):
     blue = rgbToHexHelper(b)
 
     return f"#{red}{green}{blue}"
+
+def rgbToCmyk(r, g ,b):
+    if r < 0 or r > 255 or g < 0 or g > 255 or b < 0 or b > 255:
+        return None
+    
+    r = r / 255
+    g = g / 255
+    b = b / 255
+    
+    cyan = 1 - r
+    magenta = 1 - g
+    yellow = 1 - b
+    key = min(cyan, magenta, yellow)
+        
+    if (cyan - key == 0 or 1 - key == 0):
+        cyan = 0
+    else:
+        cyan = (cyan - key) /  (1 - key)
+    if (magenta - key == 0 or 1 - key == 0):
+        magenta = 0
+    else:    
+        magenta = (magenta - key) /  (1 - key)
+    if (yellow - key == 0 or 1 - key == 0):
+        yellow = 0
+    else:
+        yellow = (yellow - key) /  (1 - key)
+
+    cyan = round(cyan,2) * 100
+    magenta = round(magenta,2) * 100
+    yellow = round(yellow,2) * 100
+    key = round(key,2) * 100
+
+    return f"cmyk({int(cyan)}%,{int(magenta)}%,{int(yellow)}%,{int(key)}%)"
  
 if __name__ == "__main__":
     while True:
-        r = int(input('RED: (0-255)'))
-        g = int(input('GREEN: (0-255)'))
-        b = int(input('BLUE: (0-255)'))
+        try:
+            converter = input('Choose converter ("1" to convert RGB to HEX, "2" to convert RGB to CMYK): ')
+            if converter == '1':
+                r = int(input('RED (0-255): '))
+                g = int(input('GREEN (0-255): '))
+                b = int(input('BLUE (0-255): '))
 
-        hex = rgbToHex(r,g,b)
-        if hex == None:
-            print('Wrong RGB input')
-        else:
-            print(f"RGB({r},{g},{b}) to HEX is: {hex}")
+                hex = rgbToHex(r,g,b)
+                if hex == None:
+                    print('Wrong RGB input')
+                else:
+                    print(f"rgb({r},{g},{b}) in HEX is: {hex}")
+            elif converter == '2':
+                r = int(input('RED: (0-255): '))
+                g = int(input('GREEN: (0-255): '))
+                b = int(input('BLUE: (0-255): '))
+
+                cmyk = rgbToCmyk(r,g,b)
+                if cmyk == None:
+                    print('Wrong RGB input')
+                else:
+                    print(f"rgb({r},{g},{b}) in CMYK is: {cmyk}")
+        except ValueError as e:
+            print(e)
--- test_color_converter.py
+++ test_color_converter.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from color_converter import rgbToHex
+from color_converter import rgbToHex, rgbToCmyk
 
 class TestRGBtoHex(unittest.TestCase):
 
@@ -14,6 +14,20 @@ class TestRGBtoHex(unittest.TestCase):
     def test_wrong_rgb_input(self):
         self.assertIsNone(rgbToHex(333,333,333))
         self.assertIsNone(rgbToHex(-1,333,333))
+
+
+class TestRGBtoCmyk(unittest.TestCase):
+
+    def test_rgb_to_cmyk(self):
+        self.assertEqual(rgbToCmyk(255,255,255), 'cmyk(0%,0%,0%,0%)')
+        self.assertEqual(rgbToCmyk(66,135,245), 'cmyk(73%,45%,0%,4%)')
+        self.assertEqual(rgbToCmyk(145,12,56), 'cmyk(0%,92%,61%,43%)')
+        self.assertEqual(rgbToCmyk(101,105,135), 'cmyk(25%,22%,0%,47%)')
+        self.assertEqual(rgbToCmyk(0,0,0), 'cmyk(0%,0%,0%,100%)')
+
+    def test_wrong_rgb_input(self):
+        self.assertIsNone(rgbToCmyk(333,333,333))
+        self.assertIsNone(rgbToCmyk(-1,333,333))
 
 if __name__ == '__main__':
     unittest.main()
